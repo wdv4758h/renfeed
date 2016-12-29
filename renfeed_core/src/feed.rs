8f8491b913f3933fc02a9de5d5555f23c3816aab
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2016-12-29 11:46:57 +0800

    feat: transform RSS/Atom feeds into internal format

--- renfeed_core/src/feed.rs
+++ renfeed_core/src/feed.rs
@@ -39,4 +39,73 @@ pub enum RawFeed {
 // Internal Handled Feed
 ////////////////////////////////////////
 
-pub struct Feed;
+#[derive(Debug, Clone, Default)]
+pub struct FeedContent {
+    pub id: String,
+    pub title: String,
+    pub date: String,
+    pub link: String,
+    pub description: String,
+    pub human_date: String,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Feed {
+    pub name: String,
+    pub feedurl: String,
+    pub homepage: String,
+    pub categories: Option<Vec<String>>,
+    pub tags: Option<Vec<String>>,
+    pub content: Vec<FeedContent>,
+}
+
+
+impl Feed {
+    pub fn from_feed_setting(setting: &FeedSetting) -> Self {
+        Feed {
+            name: setting.name.clone(),
+            feedurl: setting.feedurl.clone(),
+            homepage: setting.homepage.clone(),
+            categories: setting.categories.clone(),
+            tags: setting.tags.clone(),
+            content: vec![],
+        }
+    }
+
+    pub fn content_from_rss(&mut self, feed: RssFeed) {
+        for item in feed.items.iter() {
+            let item = item.clone();
+            self.content.push(
+                FeedContent {
+                    id: "TODO".to_string(),
+                    title: item.title.unwrap_or(Default::default()),
+                    date: item.pub_date.unwrap_or(Default::default()),
+                    link: item.link.unwrap_or(Default::default()),
+                    description: item.description.unwrap_or(Default::default()),
+                    human_date: "TODO".to_string(),
+                }
+            );
+        }
+    }
+
+    pub fn content_from_atom(&mut self, feed: AtomFeed) {
+        for item in feed.entries.iter() {
+            let item = item.clone();
+            self.content.push(
+                FeedContent {
+                    id: "TODO".to_string(),
+                    title: item.title,
+                    date: item.updated,
+                    link: item.links[0].href.clone(),
+                    description:
+                        match item.content {
+                            Some(atom_syndication::Content::Text(text)) => text,
+                            Some(atom_syndication::Content::Html(text)) => text,
+                            _ => Default::default(),
+                        },
+                    human_date: "TODO".to_string(),
+                }
+            );
+        }
+    }
+}
--- renfeed_core/src/fetch.rs
+++ renfeed_core/src/fetch.rs
@@ -1,7 +1,7 @@
 use std::io::Read;          // read_to_string
 use std::collections::HashMap;
 
-use ::feed::{FeedSettings, RawFeed, RssFeed, AtomFeed};
+use ::feed::{FeedSettings, Feed, RawFeed, RssFeed, AtomFeed};
 use hyper::Client;
 
 
@@ -22,10 +22,15 @@ pub fn get_feed(url: &str) -> RawFeed {
     }
 }
 
-pub fn get_feeds(feed_settings: &FeedSettings) -> HashMap<String, RawFeed> {
-    let mut data = HashMap::<String, RawFeed>::new();
-    for (id, feed) in feed_settings.feeds.iter() {
-        data.insert(id.clone(), get_feed(&feed.feedurl));
+pub fn get_feeds(settings: &FeedSettings) -> HashMap<String, Feed> {
+    let mut data = HashMap::new();
+    for (id, setting) in settings.feeds.iter() {
+        let mut feed = Feed::from_feed_setting(setting);
+        match get_feed(&setting.feedurl) {
+            RawFeed::RssFeed(f) => { feed.content_from_rss(f); },
+            RawFeed::AtomFeed(f) => { feed.content_from_atom(f); },
+        }
+        data.insert(id.clone(), feed);
     }
     data
 }
